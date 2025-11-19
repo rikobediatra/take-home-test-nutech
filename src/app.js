e9d96c6c84f09e3b679eaa34d95e4620578d22a0
rikobediatra <rikobediatra@gmail.com>
2025-11-19 21:33:13 +0700

    Enhance: update env

--- src/app.js
+++ src/app.js
@@ -5,14 +5,15 @@ const morgan = require('morgan');
 const cors = require('cors');
 const routes = require('./routes');
 const errorHandler = require('./middlewares/ErrorHandler');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 app.use(helmet());
-app.use(morgan("dev"));
-
+app.use(morgan('dev'));
 
 app.use(
   rateLimit({
@@ -20,6 +21,12 @@ app.use(
     max: 100,
   }),
 );
+
+const uploadDir = path.join(__dirname, '../uploads');
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir);
+}
 
 app.use(routes);
 
--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql2/promise");
+const mysql = require('mysql2/promise');
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  port: process.env.DB_PORT || 3306,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB,
+  port: process.env.MYSQL_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
